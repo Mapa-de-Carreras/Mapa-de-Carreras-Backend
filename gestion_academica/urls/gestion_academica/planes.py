29bf27dbaf0fa04e3529140b4b61322b4c2a5947
santy <cativasantiago1@gmail.com>
2025-11-04 23:19:01 -0300

    ADD: route for activate/desactivate vigencia the plan de estudio

--- gestion_academica/urls/gestion_academica/planes.py
+++ gestion_academica/urls/gestion_academica/planes.py
@@ -5,4 +5,5 @@ from gestion_academica.views.gestion_academica_views import planes
 urlpatterns = [
     path('', planes.PlanDeEstudioListCreateView.as_view(), name="plan-list-create"),
     path('<int:pk>/', planes.PlanDeEstudioDetailView.as_view(), name="plan-detail"),
+    path("<int:pk>/vigencia/", planes.PlanDeEstudioVigenciaView.as_view(), name="plan-vigencia"),
 ]

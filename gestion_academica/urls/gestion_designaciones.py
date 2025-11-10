c42c188c24880ae3c970f7503e4ee4a195670bc0
santy <cativasantiago1@gmail.com>
2025-11-10 01:15:03 -0300

    FIX: agrego las rutas de los endpoints para comision

--- gestion_academica/urls/gestion_designaciones.py
+++ gestion_academica/urls/gestion_designaciones.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path,include
 from gestion_academica.views import (
     ComisionListCreateView,
     ComisionDetailView
@@ -7,20 +7,13 @@ from rest_framework.routers import DefaultRouter
 from gestion_academica.views.gestion_designaciones_views.designaciones_docentes import DesignacionViewSet
 from gestion_academica.views.gestion_designaciones_views.cargos import CargoViewSet
 
+router = DefaultRouter()
+router.register(r"designaciones-docentes", DesignacionViewSet,
+                basename="designacion-docente")
+router.register(r"cargos", CargoViewSet, basename="cargo")
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('comisiones/', ComisionListCreateView.as_view(), name='comision_list_create'),
     path('comisiones/<int:pk>/', ComisionDetailView.as_view(), name='comision_detail'),
 ]
-
-
-
-router = DefaultRouter()
-
-# ruta para parametros de regimen
-router.register(r"designaciones-docentes", DesignacionViewSet,
-                basename="designacion-docente")
-
-# ruta para cargos
-router.register(r"cargos", CargoViewSet, basename="cargo")
-
-urlpatterns = router.urls

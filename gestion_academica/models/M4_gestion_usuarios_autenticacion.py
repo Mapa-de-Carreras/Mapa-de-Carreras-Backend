18bfb493b6de19e3672d82f3524066d0ca6745c6
santy <cativasantiago1@gmail.com>
2025-11-11 23:18:39 -0300

    ADD: modify CarreraCoordinacion model to include 'creado_por' field and adjust 'coordinador' field

--- gestion_academica/models/M4_gestion_usuarios_autenticacion.py
+++ gestion_academica/models/M4_gestion_usuarios_autenticacion.py
@@ -126,11 +126,13 @@ class CarreraCoordinacion(models.Model):
     Mantiene fecha inicio/fin, activo y quien asignó/guardó la relación.
     """
     carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
-    coordinador = models.ForeignKey("Coordinador", on_delete=models.CASCADE,related_name="coordinaciones_creadas")
+    coordinador = models.ForeignKey("Coordinador", on_delete=models.CASCADE)    
     fecha_inicio = models.DateField(default=timezone.now)
     fecha_fin = models.DateField(null=True, blank=True)
     activo = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    creado_por = models.ForeignKey(Usuario, on_delete=models.SET_NULL,
+                                   null=True, blank=True, related_name="coordinaciones_creadas")
 
     class Meta:
         constraints = [

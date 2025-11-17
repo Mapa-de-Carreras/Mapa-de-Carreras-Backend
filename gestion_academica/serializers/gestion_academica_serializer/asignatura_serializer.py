fab187cc1b61f4a0d82adc4c98b1f19ee1387a7b
unknown <yatenbueno@gmail.com>
2025-11-16 23:14:28 -0300

    fix: agregar AsignaturaConCorrelativaSerializer a '/gestion_acadmica_serializer/asignatura_serializer

--- gestion_academica/serializers/gestion_academica_serializer/asignatura_serializer.py
+++ gestion_academica/serializers/gestion_academica_serializer/asignatura_serializer.py
@@ -11,4 +11,53 @@ class AsignaturaSerializer(serializers.ModelSerializer):
             "tipo_asignatura", "tipo_duracion",
             "created_at", "updated_at"
         ]
-        read_only_fields = ["id", "created_at", "updated_at","activo"]
+        read_only_fields = ["id", "created_at", "updated_at", "activo"]
+
+
+class AsignaturaConCorrelativasSerializer(serializers.ModelSerializer):
+    correlativas = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Asignatura
+        fields = [
+            "id", "codigo", "nombre", "activo", "cuatrimestre",
+            "tipo_asignatura", "tipo_duracion",
+            "created_at", "updated_at",
+            "correlativas"
+        ]
+
+    def get_correlativas(self, asignatura):
+        """Busca correlativas usando PlanAsignatura → Correlativa."""
+        plan = self.context.get("plan")  # lo pasamos desde el serializer padre
+        if not plan:
+            return []
+
+        # buscamos el PlanAsignatura correspondiente
+        try:
+            pa = PlanAsignatura.objects.get(
+                plan_de_estudio=plan, asignatura=asignatura
+            )
+        except PlanAsignatura.DoesNotExist:
+            return []
+
+        # obtenemos correlativas reales (asignaturas)
+        correlativas = pa.correlativas_requeridas.select_related(
+            "correlativa_requerida__asignatura"
+        )
+
+        return [
+            AsignaturaSerializer(c.correlativa_requerida.asignatura).data
+            for c in correlativas
+        ]
+
+class CorrelativaSerializer(serializers.ModelSerializer):
+    plan_asignatura = serializers.PrimaryKeyRelatedField(
+        queryset=PlanAsignatura.objects.all(),
+    )
+    correlativa_requerida = serializers.PrimaryKeyRelatedField(
+        queryset=PlanAsignatura.objects.all(),
+    )
+
+    class Meta:
+        model = Correlativa
+        fields = ["id", "plan_asignatura", "correlativa_requerida"]

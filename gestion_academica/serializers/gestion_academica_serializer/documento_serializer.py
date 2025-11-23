3e2b486b822c616fcc16d986595974bd7fe84232
santy <cativasantiago1@gmail.com>
2025-11-23 01:04:32 -0300

    ADD: documentDetailSerializer with route absolute

--- gestion_academica/serializers/gestion_academica_serializer/documento_serializer.py
+++ gestion_academica/serializers/gestion_academica_serializer/documento_serializer.py
@@ -1,16 +1,25 @@
 from rest_framework import serializers
 from gestion_academica.models import Documento
 
-class ArchivoUploadSerializer(serializers.Serializer):
-    ruta = serializers.CharField()
-    archivo = serializers.FileField()
+        
+class DocumentoDetailSerializer(serializers.ModelSerializer):
+    archivo_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Documento
+        fields = ["id", "tipo", "emisor", "numero", "anio", "archivo", "archivo_url", "created_at"]
+        read_only_fields = ["archivo_url", "created_at"]
+
+    def get_archivo_url(self, obj):
+        if obj.archivo:
+            request = self.context.get("request")
+            return request.build_absolute_uri(obj.archivo.url)
+        return None
+    
+    
     
     
 class DocumentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Documento
-        fields = [
-            "id", "tipo", "emisor", "numero", "anio",
-            "archivo", "created_at"
-        ]
-        read_only_fields = ["id", "created_at"]
+        fields = ["id", "tipo", "emisor", "numero", "anio", "created_at"]

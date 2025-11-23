5148f2d2df82f8f074dffe6e29f90aaa6c6dbcb1
santy <cativasantiago1@gmail.com>
2025-11-23 01:05:39 -0300

    REMOVE: slash

--- gestion_academica/urls/gestion_academica/documentos.py
+++ gestion_academica/urls/gestion_academica/documentos.py
@@ -4,6 +4,6 @@ from django.urls import path
 from gestion_academica.views.gestion_academica_views.documentos import DocumentoListCreateView, DocumentoDetailView
 
 urlpatterns = [
-    path("/", DocumentoListCreateView.as_view(), name="documentos"),
-    path("/<int:pk>/", DocumentoDetailView.as_view(), name="documento-detalle"),
+    path('', DocumentoListCreateView.as_view(), name="documentos"),
+    path('<int:pk>/', DocumentoDetailView.as_view(), name="documento-detalle"),
 ]
